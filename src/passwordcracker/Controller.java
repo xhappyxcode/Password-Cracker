773882c09fb2237c3e58e9de11934ae35b029d10
SantillanaJasmin <jasminsantillana_cse@yahoo.com>
2016-08-11 00:44:53 +0800

    controller

--- src/passwordcracker/Controller.java
+++ src/passwordcracker/Controller.java
@@ -5,14 +5,20 @@
  */
 package passwordcracker;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 import model.User;
 
-/**
- *
- * @author Joy
- */
 public class Controller {
     private final int LIMIT = 1000;
     private final String SEMI_COLON = ":";
@@ -23,24 +29,27 @@ public class Controller {
     
     private ArrayList<User> users;
     
+    private String result;
+    
     public Controller() {
         users = new ArrayList<User>();
     }
     
     /* open and read files then save data */
     public void readFiles() throws IOException {
-        String passwd_path = "src\\textFiles\\passwd.txt";
+        String passwd_path = "passwd.txt";
         ReadFile readPass = new ReadFile(passwd_path);
         passwdData = readPass.OpenFile();
         
-        String shadow_path = "src\\textFiles\\shadow.txt";
+        String shadow_path = "shadow.txt";
         ReadFile readShad = new ReadFile(shadow_path);
         shadowData = readShad.OpenFile();
         
-        String dictionary_path = "src\\textFiles\\dictionary.txt";
+        String dictionary_path = "dictionary.txt";
         ReadFile readDict = new ReadFile(dictionary_path);
         dictionaryData = readDict.OpenFile();
     }
+    
     
     public void readPasswdData() {
         String currentLine;
@@ -78,13 +87,47 @@ public class Controller {
                 user = users.get(currUser);
                 if(user.getUsername().contentEquals(username)) {
                     user.setPassword(password);
-                    System.out.println(user.toString());
+//                    System.out.println(user.getPassword());
                 }
                 currUser++;
             }
         }
     }
-
+    
+    public void crackPassword() {
+        
+       String currentLine;
+       StringBuilder sb = new StringBuilder();
+       boolean matched = false;
+       int currUser =0;
+       User user;
+       
+       while(currUser < users.size()){
+    	   user = users.get(currUser);
+           if(user.getPassword().equals("*") || user.getPassword().equals("!") || user.getPassword().equals("x")){
+        	   matched = true;
+           } else{
+                for(int i =0; i < dictionaryData.size(); i++) {
+                    currentLine = dictionaryData.get(i);
+                    if(Sha512Crypt.verifyPassword(currentLine,user.getPassword()) == true){
+                        user.setPassword(currentLine);
+                        sb.append("Fullname: " + users.get(currUser).getFullname() + "\t\t\tUsername: " + users.get(currUser).getUsername() + "\t\t\tPassword: " + dictionaryData.get(i) + "\n");
+                        matched = true;
+                        break;
+                    }
+                }
+                
+           }
+           if(matched == false) {
+         	   sb.append("Fullname: " + users.get(currUser).getFullname() + "\t\t\tUsername: " + users.get(currUser).getUsername() + "\t\t\tPassword not found!\n");
+            }
+           
+           currUser++;
+   		}
+       
+       result = sb.toString();
+     }
+    
     /**
      * @return the passwdData
      */
@@ -106,5 +149,29 @@ public class Controller {
         return dictionaryData;
     }
     
+    public String getResult() {
+    	return this.result;
+    }
+    
+    public void createFile(String text) {
+    	JFrame parentFrame = new JFrame();
+    	 
+    	JFileChooser fileChooser = new JFileChooser(new File("C:\\"));
+    	fileChooser.setDialogTitle("Specify a file to save");   
+    	fileChooser.setFileFilter(new FileTypeFilter(".txt", "Text File"));
+    	int userSelection = fileChooser.showSaveDialog(parentFrame);
+    	 
+    	if (userSelection == JFileChooser.APPROVE_OPTION) {
+    	    File fileToSave = fileChooser.getSelectedFile();
+    	    try {
+    	    	FileWriter fw = new FileWriter(fileToSave.getPath());
+    	    	fw.write(text);
+    	    	fw.flush();
+    	    	fw.close();
+    	    } catch(Exception e2) {
+    	    	JOptionPane.showMessageDialog(null, e2.getMessage());
+    	    }
+    	}   	
+    }
     
 }
