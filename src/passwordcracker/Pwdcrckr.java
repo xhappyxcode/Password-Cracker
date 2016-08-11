3c723502aa8efcd02f844b9478255615d464333b
SantillanaJasmin <jasminsantillana_cse@yahoo.com>
2016-08-12 03:52:34 +0800

    zzzzz

--- src/passwordcracker/Pwdcrckr.java
+++ src/passwordcracker/Pwdcrckr.java
@@ -5,19 +5,19 @@ import view.MainFrame;
 
 public class Pwdcrckr {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub	
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Controller mc = new Controller();
-					mc.readFiles();
-					MainFrame frame = new MainFrame(mc);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub	
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Controller mc = new Controller();
+                    mc.readFiles();
+                    MainFrame frame = new MainFrame(mc);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
 }
